3b13f4d942f65b9e715d33b0ef25e917a755f7dc
santtir <santireyes4.sr@gmail.com>
2021-10-11 18:52:40 -0300

    hola

---
+++ js/main.js
@@ -0,0 +1,18 @@
+"use strict"
+let btn = document.querySelector("#btn-enviar");
+btn.addEventListener("click", verificarformulario);
+
+let respuesta = document.querySelector("#respuesta");
+
+
+function verificarformulario() {
+    document.querySelector("#info").innerHTML = "Gracias por su opinion.";
+    document.querySelector("#afavor").setAttribute("disabled", "");
+    document.querySelector("#encontra").setAttribute("disabled", "");
+    btn.setAttribute("disabled", "");
+
+}
+
+
+
+
